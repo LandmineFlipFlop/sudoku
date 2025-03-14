74e8259ea53e7b131d2d5656ecce0875dc63f7a8
Ely May <elystanmay@gmail.com>
2025-03-14 11:33:43 -0700

    fixed bad code paste <(•ω•)>

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,6 @@
 import sys, time, copy
 sys.path.append("../useful_functions.py")
 from useful_functions import *
-with open ('_data/test_data', 'r') as casefile:
-    lines = casefile.read().splitlines()
 startTime = time.time()
 count = 0
 
